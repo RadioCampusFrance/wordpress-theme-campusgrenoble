cae98f7e6b05653cf931878ea536baf5e467c1b8
Martin Kirchgessner <martin.kirch@gmail.com>
2016-08-05 14:25:59 +0200

    page d'archives: affichage du titre "Podcast" quand c'est des podcasts

--- archive.php
+++ archive.php
@@ -3,7 +3,7 @@
 <div class="wrapper section medium-padding">
 
 	<div class="page-title section-inner">
-		
+
 		<?php if ( is_day() ) : ?>
 			<h5><?php _e( 'Date', 'baskerville' ); ?></h5> <h3><?php echo get_the_date(); ?></h3>
 		<?php elseif ( is_month() ) : ?>
@@ -17,62 +17,64 @@
 		<?php elseif ( is_author() ) : ?>
 			<?php $curauth = (isset($_GET['author_name'])) ? get_user_by('slug', $author_name) : get_userdata(intval($author)); ?>
 			<h5><?php _e( 'Author', 'baskerville' ); ?></h5> <h3><?php echo $curauth->display_name; ?></h3>
+		<?php elseif ( get_post_type() == 'podcast' ) : ?>
+			<h3><?php _e( 'Podcasts', 'baskerville' ); ?></h3>
 		<?php else : ?>
 			<h5><?php _e( 'Archive', 'baskerville' ); ?></h5>
 		<?php endif; ?>
-		
+
 		<?php
 			$tag_description = tag_description();
 			if ( ! empty( $tag_description ) )
 				echo apply_filters( 'tag_archive_meta', '<div class="tag-archive-meta">' . $tag_description . '</div>' );
 		?>
-		
+
 	</div> <!-- /page-title -->
-	
+
 	<div class="content section-inner">
-	
+
 		<?php if ( have_posts() ) : ?>
-	
+
 			<div class="posts">
-			
+
 				<?php rewind_posts(); ?>
-			
+
 				<?php while ( have_posts() ) : the_post(); ?>
-				
+
 					<div class="post-container">
-				
+
 						<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-				
+
 							<?php get_template_part( 'content', get_post_format() ); ?>
-							
+
 							<div class="clear"></div>
-							
+
 						</div> <!-- /post -->
-					
+
 					</div>
-					
+
 				<?php endwhile; ?>
-							
+
 			</div> <!-- /posts -->
-						
+
 			<?php if ( $wp_query->max_num_pages > 1 ) : ?>
-			
+
 				<div class="archive-nav">
-				
+
 					<?php echo get_next_posts_link( '&laquo; ' . __('Older posts', 'baskerville')); ?>
-						
+
 					<?php echo get_previous_posts_link( __('Newer posts', 'baskerville') . ' &raquo;'); ?>
-					
+
 					<div class="clear"></div>
-					
+
 				</div> <!-- /post-nav archive-nav -->
-				
+
 				<div class="clear"></div>
-				
+
 			<?php endif; ?>
-					
+
 		<?php endif; ?>
-	
+
 	</div> <!-- /content -->
 
 </div> <!-- /wrapper -->
